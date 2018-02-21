b916bf90b5b54195543a13aea8ae5865907bf78b
servak <fservak@gmail.com>
2018-02-21 21:18:58 +0900

    update db path

--- db.js
+++ db.js
@@ -2,8 +2,9 @@ const Promise = require('bluebird');
 const path = require('path');
 const sqlite = require('sqlite');
 const migrationsPath = path.join(__dirname, 'migrations')
+const databasePath = path.join(__dirname, 'db', 'database.sqlite')
 const dbPromise = Promise.resolve()
-  .then(() => sqlite.open('./database.sqlite', { Promise }))
+  .then(() => sqlite.open(databasePath, { Promise }))
   .then(db => db.migrate({migrationsPath: migrationsPath}));
   // .then(db => db.migrate({ force: 'last' }));
 
